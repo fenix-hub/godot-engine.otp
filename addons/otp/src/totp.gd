4ff40f1fc7d96f7d471fb5e45f0a05c724797e0f
fenix-hub <n.santilio97@gmail.com>
2022-09-13 19:16:16 +0200

    update for 4.x

--- addons/otp/src/totp.gd
+++ addons/otp/src/totp.gd
@@ -7,12 +7,12 @@ var timestep: int = 30
 var secs_remaning: int = 0
 
 func _init(timestep: int = 30, t0: int = 0):
-    self.timestep = timestep
-    self.t0 = t0
+	self.timestep = timestep
+	self.t0 = t0
 
-func generate_totp(secret: PoolByteArray, epoch: int = Time.get_unix_time_from_system()) -> String:
-    return generate_hotp(secret, calc_time(epoch, t0, timestep))
+func generate_totp(secret: PackedByteArray, epoch: int = step_decimals(Time.get_unix_time_from_system())) -> String:
+	return generate_hotp(secret, calc_time(epoch, t0, timestep))
 
 func calc_time(time: int, t0: int, timestep: int) -> int:
-    self.secs_remaning = timestep - (time % timestep)
-    return int((time - t0) / timestep)
+	self.secs_remaning = timestep - (time % timestep)
+	return int((time - t0) / timestep)
