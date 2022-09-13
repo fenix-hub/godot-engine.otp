8ed49e0e5611c67ee167da518fd272f558301c9d
Nicolò Santilio <n.santilio97@gmail.com>
2022-09-13 18:08:10 +0000

    Update totp.gd

--- addons/otp/src/totp.gd
+++ addons/otp/src/totp.gd
@@ -10,7 +10,7 @@ func _init(timestep: int = 30, t0: int = 0):
 	self.timestep = timestep
 	self.t0 = t0
 
-func generate_totp(secret: PackedByteArray, epoch: int = step_decimals(Time.get_unix_time_from_system())) -> String:
+func generate_totp(secret: PackedByteArray, epoch: int = int(Time.get_unix_time_from_system())) -> String:
 	return generate_hotp(secret, calc_time(epoch, t0, timestep))
 
 func calc_time(time: int, t0: int, timestep: int) -> int:
